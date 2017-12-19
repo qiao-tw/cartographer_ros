8b5bbc5931815d4a4450e77ae48670ae6db2df9f
Jimmy Yen <tw.haoto@gmail.com>
2017-12-19 14:22:26 +0800

    Reduce loop closure search range in localization mode.

--- cartographer_ros/configuration_files/itri_localization.lua
+++ cartographer_ros/configuration_files/itri_localization.lua
@@ -3,4 +3,9 @@ include "itri_slam.lua"
 TRAJECTORY_BUILDER.pure_localization = true
 POSE_GRAPH.optimize_every_n_nodes = 100
 
+POSE_GRAPH.constraint_builder.max_constraint_distance = 10.
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher_3d.linear_xy_search_window = 5.
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher_3d.linear_z_search_window = 5.
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher_3d.angular_search_window = math.rad(10.)
+
 return options
